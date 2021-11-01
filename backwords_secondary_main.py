05c20ae45ea8a6281d908c2c440461f8383ee1d5
Chuanwang WANG <chaunecy@outlook.com>
2021-11-01 15:04:06 +0800

    feat(hits): add auto_hits

--- backwords_secondary_main.py
+++ backwords_secondary_main.py
@@ -3,6 +3,7 @@ This file is the portal of secondary training for backwords
 """
 import argparse
 import json
+import math
 import os.path
 import pickle
 import random
@@ -15,7 +16,8 @@ from lib4mc.FileLib import wc_l
 from lib4mc.MonteCarloLib import MonteCarloLib
 
 
-def secondary_cracker(backwords, words, config, guess_number_threshold, **kwargs):
+def secondary_cracker(backwords, words, config,
+                      func_threshold: Tuple[int, int], **kwargs):
     save_in_folder = kwargs['save']
     tag = kwargs['tag']
     nwords_dict, _words = backwords_counter(
@@ -25,7 +27,8 @@ def secondary_cracker(backwords, words, config, guess_number_threshold, **kwargs
         nwords_dict=backwords, words=words, threshold=kwargs['threshold'])
     fmodel = os.path.join(save_in_folder, f"model-to-crack-{tag}.pickle")
     with open(fmodel, 'wb') as fd:
-        config['training_list'].append(f"{guess_number_threshold}")
+        sign = kwargs['sign']
+        config['training_list'].append(f"{sign}")
         pickle.dump((nwords_dict, words, config), file=fd)
     backword_mc = BackWordsSecondaryMonteCarlo((nwords_dict, _words, config), max_iter=kwargs['max_iter'])
     # Note: this part is to "generate" some guesses and crack passwords in the testing dataset
@@ -55,6 +58,8 @@ def secondary_cracker(backwords, words, config, guess_number_threshold, **kwargs
     already_cracked = kwargs['already_cracked']
     cum: List[Tuple[str, float, int, int]] = kwargs['cum']
     with open(fcracked, 'w') as fout:
+        unique, max_gn = 0, 0
+        gn_upper_bound, hits_upper_bound = func_threshold
         for pwd, prob, num, gn, cracked, ratio in gc:
             _pwd = kwargs['splitter'].join(pwd)
             if _pwd in already_cracked:
@@ -62,7 +67,9 @@ def secondary_cracker(backwords, words, config, guess_number_threshold, **kwargs
             valid1 = (using_sample_attack and _pwd in sampled_pwds)
             if valid1:
                 gn = sampled_pwds[_pwd]
-            if valid1 or (not using_sample_attack and gn <= guess_number_threshold and 0 < prob < 1000):
+            if valid1 or (not using_sample_attack and (gn < gn_upper_bound and unique < hits_upper_bound)):
+                unique += 1
+                max_gn = max(max_gn, gn)
                 secondary_training.extend([_pwd] * num)
                 cum.append((_pwd, prob, num, gn))
                 fout.write(f"{_pwd}\t{prob:.8f}\t{num}\t{gn}\n")
@@ -78,7 +85,7 @@ def secondary_cracker(backwords, words, config, guess_number_threshold, **kwargs
             for pwd in secondary_training:
                 fout.write(f"{pwd}\n")
             pass
-    return nwords_dict, _words, config, secondary_training
+    return nwords_dict, _words, config, secondary_training, max_gn
 
 
 def wrapper():
@@ -89,11 +96,14 @@ def wrapper():
                      help="The testing file, each password a line")
     cli.add_argument("-s", "--save", dest="save", required=True, type=str,
                      help='A folder, results will be saved in this folder')
-    cli.add_argument("-g", "--guess-number-thresholds", dest="guess_number_thresholds", required=False,
-                     type=int, nargs="+",
-                     help="Each threshold refers to a guess number threshold. "
-                          "The model will crack passwords under the threshold "
-                          "and use the cracked passwords as secondary training file")
+    cli.add_argument("--strategy", dest="strategy", required=True, type=str, nargs="+",
+                     # choices=['guesses', 'hits', 'samples'],
+                     help='`guesses <guesses1> <guesses2> ...` means guess number thresholds, '
+                          '`hits <cracked1> <cracked2>` means cracked passwords, '
+                          '`auto_hits <factor> <base> <termination>` means auto generate '
+                          '<cracked1 = factor * base> <cracked2> <cracked2 = factor ** 2 * base>'
+                          '`samples <rounds>` means the number of iterations of'
+                          'Monte Carlo simulation')
     cli.add_argument("--size", dest="size", type=int, required=False, default=100000, help="sample size")
     cli.add_argument("--secondary-sample", dest="secondary_sample", type=int, required=False, default=10000000000,
                      help="use some of the cracked passwords for secondary training.")
@@ -112,10 +122,53 @@ def wrapper():
                      help="grams whose frequencies less than the threshold will be ignored")
     cli.add_argument("--max-iter", dest="max_iter", required=False, default=10 ** 20, type=int,
                      help="max iteration when calculating the maximum probability of a password")
-    cli.add_argument("--using-samples", dest='using_sample_attack', required=False, type=int, default=1,
-                     help="set the number of iterations if you want generate random passwords based on Monte Carlo "
-                          "to crack passwords in the testing dataset")
     args = cli.parse_args()
+    strategy_value = args.strategy
+    strategy = strategy_value[0]
+    permits = {'guesses', 'hits', 'samples', 'auto_hits'}
+    if strategy not in permits:
+        print(f"strategy should be one of `{', '.join(permits)}`", file=sys.stderr)
+        return
+    if len(strategy_value) < 2:
+        print(f"strategy should have at least 2 values", file=sys.stderr)
+        return
+
+    using_sample_attack, signs = False, []
+    upper_bound, hits_upper_bound = 10 ** 14, 10 ** 14
+    func_thresholds = []
+    if strategy == 'guesses':
+        print(f"using guesses", file=sys.stderr)
+        values = strategy_value[1:]
+        values = [int(v) for v in values]
+        for i, v in enumerate(values):
+            func_thresholds.append((v, hits_upper_bound))
+            signs.append(f"guesses-{v:,}")
+        pass
+    elif strategy == 'hits':
+        print(f"using hits", file=sys.stderr)
+        values = strategy_value[1:]
+        values = [int(v) for v in values]
+        for i, v in enumerate(values):
+            func_thresholds.append((upper_bound, v))
+            signs.append(f"hits-{v:,}")
+        pass
+    elif strategy == 'auto_hits':
+        print(f"using auto_hits", file=sys.stderr)
+        factor, base, termination = int(strategy_value[1]), int(strategy_value[2]), int(strategy_value[3])
+        end = math.ceil(math.log(termination / max(base, 1)) / math.log(max(factor, 1)))
+        for i, v in enumerate(range(1, end)):
+            nv = (factor ** v) * base
+
+            func_thresholds.append((upper_bound, nv))
+            signs.append(f"auto_hits-{v:,}")
+    else:
+        print(f"using samples", file=sys.stderr)
+        v = int(strategy[1])
+        func_thresholds = [(upper_bound, hits_upper_bound) for _ in range(v)]
+        signs = [f"samples-{args.size}" for _ in range(v)]
+        using_sample_attack = True
+        pass
+    rounds = len(func_thresholds)
     splitter_map = {'empty': '', 'space': ' ', 'tab': '\t'}
     if args.splitter.lower() in splitter_map:
         args.splitter = splitter_map[args.splitter.lower()]
@@ -127,36 +180,32 @@ def wrapper():
     if not os.path.exists(args.save):
         os.mkdir(args.save)
     already_cracked = set()
-    guess_number_thresholds: List[int] = args.guess_number_thresholds
-    if guess_number_thresholds is not None:
-        rounds = len(guess_number_thresholds)
-        print(f"Guess number mode", file=sys.stderr, end=', ')
-        using_sample_attack = False
-    else:
-        rounds = args.using_sample_attack
-        using_sample_attack = True
-        guess_number_thresholds = [args.size] * rounds
-        print(f"Sample mode", file=sys.stderr)
+
     print(f"We will have {rounds} rounds", file=sys.stderr, end=', ')
     cums: List[List[Tuple[str, float, int, int]]] = []
+    max_guess_numbers = []
     for idx in range(rounds):
         # guess_number_threshold have default value of [args.size, ..., args.size] if it is None
-        guess_number_threshold = guess_number_thresholds[idx]
+        func_threshold = func_thresholds[idx]
         # Therefore, prior_guesses will always be args.size if `--using-samples`
         print(f"The {idx}-th iteration", file=sys.stderr)
         cum = []
-        backwords, words, config, training = secondary_cracker(
+        backwords, words, config, training, max_gn = secondary_cracker(
             backwords, words, config=config,
-            guess_number_threshold=guess_number_threshold,
+            func_threshold=func_threshold,
             training=training, splitter=args.splitter,
             start4words=args.start4words, skip4words=args.skip4words,
             max_gram=args.max_gram, size=args.size, max_iter=args.max_iter,
             testing=args.testing, save=args.save, secondary_sample=args.secondary_sample,
             already_cracked=already_cracked, cum=cum,
-            threshold=args.threshold,
+            threshold=args.threshold, sign=signs[idx],
             using_sample_attack=using_sample_attack, tag=f"iter-{idx}",
         )
         cums.append(cum)
+        max_guess_numbers.append(max_gn)
+        if max_gn >= upper_bound:
+            print(f"Too large guess number reached: {max_gn}, the training process is terminated", file=sys.stderr)
+            break
         pass
     backwords, words = backwords_counter(
         training, splitter=args.splitter, start_chr=start_chr, end_chr=end_chr,
@@ -189,7 +238,7 @@ def wrapper():
     with open(f_sectional_result, "w") as fout_sectional_result:
         _cracked = 0
         _total = wc_l(args.testing)
-        for gnt, cum in zip([0, *guess_number_thresholds], cums):
+        for gnt, cum in zip([0, *max_guess_numbers], cums):
             for (_pwd, _prob, _n, _gn) in cum:
                 _cracked += _n
                 _ratio = _cracked / _total * 100
